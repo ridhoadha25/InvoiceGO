c890e064afab881dd2f128d9c7798e1bed601c6e
M.Ridho Adha <mridhoadha25@gmail.com>
2026-08-01 16:35:01 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Invoice from "./pages/Invoice";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
 
-export default function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/invoice" element={<Invoice />} />
-    </Routes>
-  );
-}
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
